63e9635a674630463894fff16a2bb87fd2e5add9
Okano Takumi <oknokn.of.12@gmail.com>
2020-04-13 15:30:37 +0900

    commit practice ruby on rails app

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+ #passと確認pass一致チェック
+ has_secure_password
+
+ #validation check
+ validates :name
+   uniqueness: true,
+   presense: true,
+   length: { maximum: 16 }
+   format: {
+     with: /\A[a-z0-9]+\z,
+     message: 'は小文字英数字で入力してください'
+   }
+
+ validates :password
+   length: { minimum: 8 }
+   format: {
+     with: /\A[A-Za-z0-9]+\z,
+     message: 'は大文字小文字英数字を含んだ８文字以上である必要があります'
+end
---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  #get 'my page', to: 'users#me'
+  #post 'login', to: 'sessions#create'
+  #delete 'logout', to: 'sessions#destroy'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #resources :users, only: %i[new create]
+  #root 'home#index'
+  #get 'boards', to: 'boards#index'
+  #get 'boards/new', to: 'boards#new'
+  #post 'boards', to: 'boards#create'
+  #get 'boards/:id', to: 'boards#show'
+  resources :boards
+end
